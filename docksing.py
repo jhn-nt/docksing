edbdc43b087f38d689e8dd3b585347c7cf415517
jhn-nt <giovanni.angelotti@supsi.ch>
2024-09-19 18:33:00 +0000

    volume mapping

--- docksing.py
+++ docksing.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 from paramiko import SSHClient, AutoAddPolicy, SFTPClient
 from scp import SCPClient
 import docker
+from tqdm import tqdm
 from docker.client import DockerClient
 from getpass import getpass
 from functools import partial
@@ -11,6 +12,7 @@ import subprocess
 import os
 from pathlib import Path
 from typing import List
+import warnings
 
 
 @dataclass
@@ -72,6 +74,7 @@ class CLICompose:
 
                     cmd+=[f"--env {k}={v}" for (k,v) in item.items()]
             elif key=="volumes":
+                warnings.warn("Consider passing data thorugh APIs instead of volumes.")
                 cmd+=[f"--volume {vol}" for vol in item]
             elif key=="ports":
                 cmd+=[f"-p {p}" for p in item]
@@ -112,7 +115,6 @@ class DockSing:
     def local(cls)->DockSing:
         return cls(ssh=None,docker=docker.from_env())
     
-
     def setup(self,remotedir:str):
         """Asserts whether `remotedir` exists in remote host and otherwise creates it.
 
@@ -125,8 +127,6 @@ class DockSing:
         else:
             _=os.mkdir(Path.cwd() / remotedir)
 
-
-    
     def push(self,tag:str,remotedir:str):
         """Pushes the target oci image `image` from the local docker daemon to the remote host as a `.tar` archive file in `remotedir`.
 
@@ -149,8 +149,50 @@ class DockSing:
                 with open(f"{remotedir}/{iid}.tar","wb") as f:
                     f.write(file.getbuffer())
     
+    def remote_volume(self,remote_dir:str,local_dir:str,container_dir:str,send_payload:bool=False)->str:
+        """If a volume mount of the form `local_dir:container_dir` is requested then `docksing` sends the content of `local_dir` via
+        scp to `remote_dir/local_dir` and submits to singularity the mapping `remote_dir/local_dir:conatainer_dir`.
 
-    
+        Args:
+            remote_dir (str): Remote direcorty as indicited in `remote_dir`
+            local_dir (str): The local directory of the volume mapping.
+            container_dir (str): The container direcotry of the volume mapping.
+
+        Returns:
+            str: The updated singularity mapping.
+        """
+        if send_payload:
+            with SCPClient(self.ssh.get_transport()) as scp:
+                scp.put(
+                    local_dir,
+                    remote_path=remote_dir,
+                    recursive=True)
+        
+        return f"{remote_dir}{local_dir}:{container_dir}"
+
+    def override_volumes(self,remote_dir:str,container_config:dict,send_payload:bool=False)->dict:
+        """Given a `container_config` file, this methods searches if a volume mapping is requested, 
+        if it does and `send_payload` is set to `True`, it starts and SCP transfer to `remote_dir`.
+        
+        Args:
+            remote_dir (str): The remote directory where to store the volumes.
+            container_config (dict): A dictionare container the docker configuration data.
+            send_payload (bool, optional): Boolean indicating the initiation of file transfer from local to remote. Defaults to False.
+
+        Returns:
+            dict: A symbolic volume mapping to eventually pass to singularity. Returns an empty dictionary if no volumes are requested in `container_config`.
+        """
+
+        if "volumes" in container_config:
+            remote_volumes=[]
+            pbar=tqdm(container_config["volumes"],total=len(container_config["volumes"]))
+            for volume in pbar:
+                local_dir,container_dir=volume.split(":")
+                remote_volumes.append(self.remote_volume(remote_dir,local_dir,container_dir,send_payload=send_payload))
+            return {"volumes":remote_volumes}
+        else:
+            return {}
+
     def build(self,tag:str,remotedir:str):
         """Generates a `.sif` file from a docker image archive file.
 
@@ -163,7 +205,6 @@ class DockSing:
             iid=image.short_id.split(":")[1]
             self.ssh.exec_command(f"cd {remotedir}; srun singularity build {iid}.sif docker-archive://{iid}.tar")
 
-
     def submit(self,tag:str,remotedir:str,container_config:List[str],slurm_config:List[str]):
         if self.ssh:
             image=self.docker.images.get(tag)
@@ -171,7 +212,7 @@ class DockSing:
 
             slurm_cmd=CLICompose.slurm_run_opt(slurm_config)
             run_cmd=CLICompose.singularity_run_opt(container_config,
-                                                   override={"image":f"{iid}.sif"},
+                                                   override={"image":f"{iid}.sif"} | self.override_volumes(remotedir,container_config,send_payload=True),
                                                    ignore=["container_name"])
             opt_cmd=CLICompose.container_opt(container_config)
             cmd=" ".join(slurm_cmd+run_cmd+opt_cmd)
@@ -189,11 +230,16 @@ class DockSing:
                                    stderr=subprocess.STDOUT,
                                    start_new_session=True)
                 
-
-    def cli(self,container_config:List[str],slurm_config:List[str]):
+    def cli(self,remotedir:str,tag:str,container_config:List[str],slurm_config:List[str]):
         if self.ssh:
-            cmd=CLICompose.slurm_run_opt(slurm_config)+CLICompose.singularity_run_opt(container_config)+CLICompose.container_opt(container_config)
-            cmd=" ".join(cmd)
+            image=self.docker.images.get(tag)
+            iid=image.short_id.split(":")[1]
+            slurm_cmd=CLICompose.slurm_run_opt(slurm_config)
+            run_cmd=CLICompose.singularity_run_opt(container_config,
+                                                   override={"image":f"{iid}.sif"} | self.override_volumes(remotedir,container_config,send_payload=False),
+                                                   ignore=["container_name"])
+            opt_cmd=CLICompose.container_opt(container_config)
+            cmd=" ".join(slurm_cmd+run_cmd+opt_cmd)
         else:
             cmd=CLICompose.docker_run_opt(container_config)+CLICompose.container_opt(container_config)
             cmd=" ".join(cmd)
@@ -235,7 +281,7 @@ if __name__=="__main__":
         client=DockSing.connect(SSH)
 
     if CLI:
-        print(client.cli(CONFIG["container"],CONFIG["slurm"]))
+        print(client.cli(CONFIG["remotedir"],CONFIG["container"]["image"],CONFIG["container"],CONFIG["slurm"]))
     else:
         client.setup(CONFIG["remotedir"])
         client.push(CONFIG["container"]["image"],CONFIG["remotedir"])
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="docksing",
-    version="0.2.10",
+    version="0.2.14",
     description="Lightweight Docker to Singularity to HPC Deployer",
     author="G. Angelotti",
     author_email="giovanni.angelotti@idsia.ch",

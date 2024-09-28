9fcbbd35d9ca1443fe46a7d5a3431f8010b91b5a
Giovanni Angelotti <giovanni.angelotti@idsia.ch>
2024-09-28 12:20:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="docksing",
     version="0.2.23",
     description="CLI Utility for deployment of containerized jobs on SLURM HPC ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="G. Angelotti",
     author_email="giovanni.angelotti@idsia.ch",
     py_modules=["docksing"],
